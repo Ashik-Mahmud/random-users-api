f3099a2ed1cb564ada215362211756105b8d10f6
ashikmahmud <ashikmamud187@gmail.com>
2022-02-23 23:08:23 +0600

    change something

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ TODO:
 */
 
 /* step 1 - select all important elements  */
-const cardWrapper = document.querySelector('.wrapper');
+const cardWrapper = document.querySelector('.container');
 
 /* step 2 - create a function for randomly created users  */
 const randomUsers = async () =>{
@@ -26,63 +26,64 @@ const displayUsers = (user) =>{
     // for user address 
     let {country, state, city} = user.location;
     let {number, name} = user.location.street;
-    let cartTag = `
-    <div class="avatar">
-        <img src="${user.picture.large}" alt="image for users">
-    </div>
-    <div class="user-details">
-        <div class="name-info">
-            <h2>${user.name.title} ${user.name.first} ${user.name.last}</h2>
-            <span class="email">${user.email}</span>
-        </div>
-        <div class="address">
-            <div class="address-header">
-                <ul class="header-title">
-                    <li class="tab-button active">Location</li>
-                    <li class="tab-button">Login</li>
-                    <li class="tab-button">Personal</li>
-                </ul>
-                <ul class="display-info">
-                    <li class="location">
-                        <ul>
-                            <li>Street: <span>${number}, ${name}</span></li>
-                            <li>City : <span>${city}</span> </li>
-                            <li>State : <span>${state}</span></li>
-                            <li>Country : <span>${country}</span></li>
-                        </ul>
-                    </li>
-                    <li class="login-info">
-                        <ul>
-                            <li>Cell: <span>${user.cell}</span></li>
-                            <li>Phone: <span>${user.phone}</span></li>
-                            <li>Email: <span>${user.email}</span></li>
-                            <li>Password: <span>${user.login.password}</span></li>
-                        </ul>
-                    </li>
-                    <li class="personal">
-                        <ul>
-                            <li>Date of Birth <span>${user.dob.date}</span></li>
-                            <li>Age <span>${user.dob.age}</span></li>
-                            <li>Gender <span>${user.gender}</span></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>`;
-    cardWrapper.innerHTML = cartTag;
+
+    const div = document.createElement('div');
+    div.classList.add('wrapper');
+    div.innerHTML =`
+                    <div class="avatar">
+                        <img src="${user.picture.large}" alt="image for users">
+                    </div>
+                    <div class="user-details">
+                        <div class="name-info">
+                            <h2>${user.name.title} ${user.name.first} ${user.name.last}</h2>
+                            <span class="email">${user.email}</span>
+                        </div>
+                        <div class="address">
+                            <div class="address-header">
+                                <ul class="header-title">
+                                    <li class="tab-button active">Location</li>
+                                    <li class="tab-button">Login</li>
+                                    <li class="tab-button">Personal</li>
+                                </ul>
+                                <ul class="display-info">
+                                    <li class="location">
+                                        <ul>
+                                            <li>Street: <span>${number}, ${name}</span></li>
+                                            <li>City : <span>${city}</span> </li>
+                                            <li>State : <span>${state}</span></li>
+                                            <li>Country : <span>${country}</span></li>
+                                        </ul>
+                                    </li>
+                                    <li class="login-info">
+                                        <ul>
+                                            <li>Cell: <span>${user.cell}</span></li>
+                                            <li>Phone: <span>${user.phone}</span></li>
+                                            <li>Email: <span>${user.email}</span></li>
+                                            <li>Password: <span>${user.login.password}</span></li>
+                                        </ul>
+                                    </li>
+                                    <li class="personal">
+                                        <ul>
+                                            <li>Date of Birth <span>${user.dob.date}</span></li>
+                                            <li>Age <span>${user.dob.age}</span></li>
+                                            <li>Gender <span>${user.gender}</span></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`;
+    cardWrapper.append(div);
 }
 
 /* step 4 now work with change tab  */
 const headerTabs = document.getElementsByClassName('tab-button');
 // const headerTitle = document.querySelectorAll(".header-title li");
 // console.log(headerTitle)
-for(const tab of headerTabs){
-    console.log(tab, 'Yeah')
-}
-console.log(headerTabs.length);
-
-
+console.log(headerTabs)
+for(const tab in headerTabs){
+    console.log(tab);
+};
 
 
 /* last step for calling all function  */
